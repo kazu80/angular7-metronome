96519f7177a6c0c8eb5e9701b0a07e0e43f758dd
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2018-12-01 19:25:17 +0900

    Load sound file on run component

--- src/app/metronome/run/run.component.ts
+++ src/app/metronome/run/run.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./run.component.scss']
 })
 export class RunComponent implements OnInit {
+  context: AudioContext;
 
   constructor() { }
 
   ngOnInit() {
+    this.context = new AudioContext();
+
+    const url = '../../../assets/sound/s_01.mp3';
+
+    const request = new XMLHttpRequest ();
+    request.open ('GET', url, true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = () => {
+      this.context.decodeAudioData (request.response,
+        function (buffer) {
+          if (!buffer) {
+            alert ('error decoding file data: ' + url);
+            return;
+          }
+
+          console.log('sound loaded.');
+        },
+        function (error) {
+          console.error ('decodeAudioData error', error);
+        }
+      );
+    };
+
+    request.onerror = function (e) {
+      console.error ('BufferLoader: XHR error:', e);
+    };
+
+    request.send ();
   }
 
   private onClick() {
