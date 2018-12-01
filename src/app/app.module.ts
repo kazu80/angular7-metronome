475ca04ea62208f0c87723489aafcc831e3d0ac6
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2018-12-01 19:04:12 +0900

    Create run component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopComponent } from './metronome/top/top.component';
+import { RunComponent } from './metronome/run/run.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopComponent
+    TopComponent,
+    RunComponent
   ],
   imports: [
     BrowserModule,
