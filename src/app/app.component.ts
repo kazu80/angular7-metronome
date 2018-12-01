8fa7da556e68957044ee3502d4e192d5f497a844
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2018-12-01 18:51:19 +0900

    edit app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import {Component, Injector} from '@angular/core';
+import {createCustomElement} from '@angular/elements';
+import {TopComponent} from './metronome/top/top.component';
 
 @Component({
   selector: 'app-metronome',
   template: ``
 })
 export class AppComponent {
-  constructor(private injector: Injector) {}
+  constructor(private injector: Injector) {
+    const AppMetronomeElement = createCustomElement(TopComponent, {
+      injector: this.injector
+    });
+    customElements.define('app-metronome', AppMetronomeElement);
+  }
 }
